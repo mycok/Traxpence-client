2226caf3dfc47763babbc16314fe7a30617431de
myko <awesomeme155@gmail.com>
2020-11-21 13:33:38 +0300

    feat: display server errors
    - add server error component
    - integrate server error component with the signup component

---
+++ src/shared/ServerMessage.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Snackbar } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => createStyles({
+  error: {
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.error.contrastText,
+  },
+}));
+
+type SnackbarProps = {
+    open: boolean,
+    message: string,
+    onClose(): void,
+}
+
+function ServerMessage({ open, message, onClose }: SnackbarProps) {
+  const classes = useStyles();
+
+  return (
+    <Snackbar
+      open={open}
+      message={message}
+      onClose={onClose}
+      autoHideDuration={3000}
+      ContentProps={{
+        classes: {
+          root: classes.error,
+        },
+        variant: 'outlined',
+      }}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    />
+  );
+}
+
+export default ServerMessage;
